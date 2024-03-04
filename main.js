6aec72bc1877031d8023c2ed1b50e2258b1944a5
gusanttoss <gustavo.rosantos9618@gmail.com>
2024-03-04 11:57:08 -0300

    alterações

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-
-    $('form').on('submit',function(e) {
-        e.preventDefault();
-
-        const novaTarefa = $('#inserir-tarefa').val();
-        const tarefaElemento = $('').text(novaTarefa); 
-
-        $('ul').prependTo(tarefaElemento); 
-
-        $('#inserir-tarefa').val('');
-    })
-
-});
+$(document).ready(function () {
+    $("form").on("submit", function (e) {
+      e.preventDefault();
+      const novaTarefa = $("#inserir-tarefa").val();
+  
+      // Verifica se a tarefa já existe na lista
+      const tarefaExiste = $("ul li")
+        .toArray()
+        .some((li) => $(li).text() === novaTarefa);
+      if (tarefaExiste) {
+        alert("Esta tarefa já existe na lista.");
+        return;
+      }
+  
+      const listaTarefas = $(`<li class="has-marker">${novaTarefa}</li>`);
+      $(listaTarefas).appendTo("ul");
+      $("#inserir-tarefa").val(""); // limpa o input após adicionar a tarefa
+    });
+  
+    $("ul").on("click", "li", function () {
+      $(this).toggleClass("strike-through");
+    });
+  });
